f851886ea83ceddb0e25f8e65b81991012d41859
Dendi <352671309@qq.com>
2022-12-30 17:59:31 +0800

    Feat: 设置默认字体 增加字体图标 重制样式

--- src/Root.vue
+++ src/Root.vue
@@ -1,10 +1,12 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <h2>根组建</h2>
   <router-view />
 </template>
 
-<style scoped></style>
+<style>
+@import '../src/assets/styl/normalize.scss';
+@import '../src/assets/styl/iconfont.css';
+@import '../src/assets/styl/common.scss';
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, 'src'),
       '@store': path.resolve(__dirname, 'src/store'),
+      '@styl': path.resolve(__dirname, 'assets/styl'),
     },
   },
   plugins: [
