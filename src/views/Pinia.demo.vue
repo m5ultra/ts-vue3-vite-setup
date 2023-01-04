a8fa8a5af3a00dde883bea59053ede013a4712eb
Dendi <352671309@qq.com>
2023-01-05 00:06:18 +0800

    Feat: 自动导入图标组建

---
+++ src/views/Pinia.demo.vue
@@ -0,0 +1,20 @@
+<template>
+  <h2>This is Home Page</h2>
+  <h3>{{ s.count }}</h3>
+  <h3>{{ s.double }}</h3>
+  <el-button type="primary" @click="handleClick">Click me!</el-button>
+  <hr />
+  <router-view />
+</template>
+
+<script setup lang="ts">
+import { useLogin, useStore } from '@store'
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  console.log(useLogin().infos, '999')
+})
+const s = useStore()
+
+const handleClick = () => s.increment()
+</script>
